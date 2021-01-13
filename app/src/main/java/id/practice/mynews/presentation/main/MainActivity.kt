4805897b381af863b5a021644c6eed438444623a
Flabib <fahdilabib@gmail.com>
2021-01-14 02:41:33 +0700

    Implementing Obfuscation, Crashlytics and Timber

--- app/src/main/java/id/practice/mynews/presentation/main/MainActivity.kt
+++ app/src/main/java/id/practice/mynews/presentation/main/MainActivity.kt
@@ -1,17 +1,15 @@
 package id.practice.mynews.presentation.main
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
-import com.google.android.play.core.splitinstall.SplitInstallRequest
 import id.practice.mynews.R
 import id.practice.mynews.core.data.Resource
 import id.practice.mynews.core.ui.ArticleAdapter
@@ -19,7 +17,6 @@ import id.practice.mynews.databinding.ActivityMainBinding
 import id.practice.mynews.presentation.detail.DetailActivity
 import org.koin.android.viewmodel.ext.android.viewModel
 
-// TODO: Move sting to xml
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModel()
     private lateinit var binding: ActivityMainBinding
@@ -63,12 +60,8 @@ class MainActivity : AppCompatActivity() {
 
                         if (it.data == null) {
                             showErrorMessage(
-                                R.drawable.oops, "Oops..",
-                                """
-                                Network failure, Please Try Again!
-                                
-                                ${it.message}
-                                """.trimIndent()
+                                R.drawable.oops, getString(R.string.template_oops_title),
+                                String.format(getString(R.string.template_oops_message), it.message)
                             )
                         }
                     }
@@ -76,12 +69,8 @@ class MainActivity : AppCompatActivity() {
                         binding.topHeadlines.visibility = View.INVISIBLE
                         binding.swipeRefreshLayout.isRefreshing = false
                         showErrorMessage(
-                            R.drawable.no_result, "No Result",
-                            """
-                            Please Try Again!
-                            
-                            ${it.message}
-                            """.trimIndent()
+                            R.drawable.no_result, getString(R.string.template_empty_title),
+                            String.format(getString(R.string.template_empty_message), it.message)
                         )
                     }
                 }
@@ -102,11 +91,8 @@ class MainActivity : AppCompatActivity() {
                 startActivity(mIntent)
             }
             R.id.favorite_btn -> {
-                try {
-                    installExtraModule()
-                } catch (e: Exception) {
-                    Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
-                }
+                val uri = Uri.parse("mynews://extra")
+                startActivity(Intent(Intent.ACTION_VIEW, uri))
             }
         }
 
@@ -122,34 +108,5 @@ class MainActivity : AppCompatActivity() {
         binding.componentError.errorTitle.text = title
         binding.componentError.errorMessage.text = message
         binding.componentError.btnRetry.setOnClickListener { }
-    }
-
-    private fun installExtraModule() {
-        val splitInstallManager = SplitInstallManagerFactory.create(this)
-        val moduleExtra = "extra"
-        if (splitInstallManager.installedModules.contains(moduleExtra)) {
-            moveToExtraActivity()
-        } else {
-            val request = SplitInstallRequest.newBuilder()
-                .addModule(moduleExtra)
-                .build()
-            splitInstallManager.startInstall(request)
-                .addOnSuccessListener {
-                    Toast.makeText(this, "Success installing module", Toast.LENGTH_SHORT).show()
-                    moveToExtraActivity()
-                }
-                .addOnFailureListener {
-                    Toast.makeText(this, "Error installing module", Toast.LENGTH_SHORT).show()
-                }
-        }
-    }
-
-    private fun moveToExtraActivity() {
-        startActivity(
-            Intent(
-                this,
-                Class.forName("id.practice.mynews.extra.presenter.ExtraActivity")
-            )
-        )
     }
 }
